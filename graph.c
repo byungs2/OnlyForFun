cf9cc554e37d0a8ffddfbfeadcdcd102af596bf2
byunghun <byunghun.lee@fainders.ai>
2021-07-22 16:41:33 +0900

    add graph, dfs, bfs

--- graph.c
+++ graph.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//TODO: BFS, DFS, Graph with Matrix & Linked List, Dijkstra, Segment & index tree, KMP, Alpha Beta pruning 
-//IMPLEMENTED: Queue, Linked_List, Stack, Quick sort, Merge sort, Priority Queue with Max heap
+//TODO: Dijkstra, Segment & index tree, KMP, Alpha Beta pruning 
+//IMPLEMENTED: Queue, Linked_List, Stack, Quick sort, Merge sort, Priority Queue with Max heap, Graph with list, BFS, DFS
+
+typedef struct _Edge {
+  int weight;
+  int end_node_index;
+  int used;
+  struct _Edge *next;
+} Edge;
+
+typedef struct _GraphNode {
+  int index;
+  int visit;
+  void (*read_edge)(struct _GraphNode* ,void *, const char *, int);
+  Edge *edge;
+} GraphNode;
+
+typedef struct _Graph {
+  GraphNode **node_list;
+  void (*push_edge)(struct _Graph*, int, int, int);
+  void (*push_node)(struct _Graph*, int);
+} Graph;
 
 typedef struct _Node {
   int index;
@@ -26,7 +46,7 @@ typedef struct _Stack{
   Node *top;
   int length;
   void (*push)(struct _Stack*, int, int);
-  void (*pop)(struct _Stack*);
+  int (*pop)(struct _Stack*);
   void (*pop_all)(struct _Stack*);
 } Stack;
 
@@ -35,7 +55,7 @@ typedef struct _Queue{
   Node *tail;
   int length;
   void (*push)(struct _Queue*, int, int);
-  void (*pop)(struct _Queue*);
+  int (*pop)(struct _Queue*);
   void (*pop_all)(struct _Queue*);
 } Queue;
 
@@ -157,15 +177,19 @@ void stack_push(Stack *stack, int data, int prior){
   } 
 }
 
-void stack_pop(Stack *stack){
+int stack_pop(Stack *stack){
+  int popped = -1;
   if(stack->top != NULL){
     printf("%d is poped\n", stack->top->data);
     Node *del = stack->top;
+    popped = del->data;
     stack->top = stack->top->next;
     stack->length--;
     free(del);
+    return popped;
   } else {
     printf("There is no poped\n");
+    return popped;
   }
 }
 
@@ -189,15 +213,19 @@ void queue_push(Queue *queue, int data, int prior){
   } 
 }
 
-void queue_pop(Queue *queue){
+int queue_pop(Queue *queue){
+  int popped = -1;
   if(queue->top != NULL){
     printf("%d is poped\n", queue->top->data);
     Node *del = queue->top;
+    popped = del->data;
     queue->top = queue->top->next;
     queue->length--;
     free(del);
+    return popped;
   } else {
     printf("There is no poped\n");
+    return popped;
   }
 }
 
@@ -215,6 +243,7 @@ void refresh_height(Prior_Queue *pq){
   } 
   pq->height = cnt;
 }
+
 void swap_array_elements(Node **array, int a, int b){
   Node *node = array[a];
   array[a] = array[b];
@@ -288,7 +317,7 @@ void prior_queue_pop_all(Prior_Queue *pq){
   }
 }
 
-void prior_queue_init(Prior_Queue *pq, int size){
+void init_prior_queue(Prior_Queue *pq, int size){
   pq->heap_array = (Node **) malloc(sizeof(Node*) * size * 2);
   for(int i = 0; i < size*2; i++){
     pq->heap_array[i] = NULL;
@@ -299,6 +328,74 @@ void prior_queue_init(Prior_Queue *pq, int size){
   pq->push = prior_queue_push;
   pq->pop = prior_queue_pop;
   pq->pop_all = prior_queue_pop_all;
+}
+
+void read_edge(GraphNode *gn, void *type, const char *type_name, int prior){
+  Edge *edge = gn->edge;
+  if(strcmp(type_name, "STACK") == 0){
+    Stack *stack = (Stack *)type;
+    while(edge != NULL){
+      if(edge->used == 0){
+        stack->push(stack, edge->end_node_index, prior);
+        edge->used = 1;
+      } else {
+        printf("This edge has been used already\n");
+      }
+      edge = edge->next;
+    }
+  } else {
+    Queue *queue = (Queue *)type;
+    while(edge != NULL){
+      if(edge->used == 0){
+        queue->push(queue, edge->end_node_index, prior);
+        edge->used = 1;
+      } else {
+        printf("This edge has been used already\n");
+      }
+      edge = edge->next;
+    }
+  } 
+}
+
+void init_graph_node(GraphNode *gn, int index){ 
+  gn->index = index;
+  gn->visit = 0;
+  gn->edge = NULL;
+  gn->read_edge = read_edge;
+}
+
+void gen_edge(Edge *edge, int weight, int end_node_index){
+  edge->weight = weight;
+  edge->end_node_index = end_node_index;
+  edge->next = NULL;
+  edge->used = 0;
+}
+
+void push_edge_to_graph(Graph *graph, int start_node_index, int weight, int end_node_index){
+  Edge *edge = (Edge *)malloc(sizeof(Edge));
+  gen_edge(edge, weight, end_node_index);
+  GraphNode *gn = graph->node_list[start_node_index];
+  if(gn->edge == NULL){
+    gn->edge = edge;
+  } else {
+    edge->next = gn->edge;
+    gn->edge = edge; 
+  }
+}
+
+void push_node_to_graph(Graph *graph, int size){
+  graph->node_list = (GraphNode **)malloc(sizeof(GraphNode*)*size);
+  for(int i = 0; i < size; i++){
+    GraphNode *gn = (GraphNode *)malloc(sizeof(GraphNode));
+    init_graph_node(gn, i);
+    graph->node_list[i] = gn;
+  }
+}
+
+void init_graph(Graph *graph){
+  graph->node_list = NULL;
+  graph->push_edge = push_edge_to_graph;
+  graph->push_node = push_node_to_graph;
 }
 
 void init_stack(Stack *stack){
@@ -315,6 +412,48 @@ void init_queue(Queue *queue){
   queue->push = queue_push;
   queue->pop = queue_pop;
   queue->pop_all = queue_pop_all;
+}
+
+void BFS(Graph *graph){
+  int next_gn_index;
+
+  GraphNode **graph_list = graph->node_list;
+  Stack *stack = (Stack *) malloc(sizeof(Stack));
+  init_stack(stack);
+
+  GraphNode *gn = graph_list[0];
+  while(1){
+    gn->read_edge(gn, stack, "STACK", 1); 
+    printf("Visit %d graph node \n", gn->index);
+    next_gn_index = stack->pop(stack);
+    if(next_gn_index != -1){
+      gn = graph_list[next_gn_index];
+    } else {
+      break;
+    }
+  }
+  free(stack);
+}
+
+void DFS(Graph *graph){
+  int next_gn_index;
+ 
+  GraphNode **graph_list = graph->node_list;
+  Queue *queue = (Queue *) malloc(sizeof(Queue));
+  init_queue(queue);
+  
+  GraphNode *gn = graph_list[0];
+  while(1){
+    gn->read_edge(gn, queue, "QUEUE", 1); 
+    printf("Visit %d graph node \n", gn->index);
+    next_gn_index = queue->pop(queue);
+    if(next_gn_index != -1){
+      gn = graph_list[next_gn_index];
+    } else {
+      break;
+    }
+  }
+  free(queue);
 }
 
 void add_to_linked_list(Linked_List *ll, int data, int prior){
@@ -407,24 +546,64 @@ int main(int argc, char *argv[]){
   Stack *stack = (Stack *) malloc(sizeof(Stack));
   Queue *queue = (Queue *) malloc(sizeof(Queue));
   Prior_Queue *prior_queue = (Prior_Queue *) malloc(sizeof(Prior_Queue));
+  Graph *graph_for_bfs = (Graph *) malloc(sizeof(Graph));
+  Graph *graph_for_dfs = (Graph *) malloc(sizeof(Graph));
+
   init_linked_list(l_list);
   init_stack(stack);
   init_queue(queue);
-  prior_queue_init(prior_queue, 10);
+  init_prior_queue(prior_queue, 10);
+  init_graph(graph_for_bfs);
+  init_graph(graph_for_dfs);
+
   printf("init complete \n");
+
+  graph_for_dfs->push_node(graph_for_dfs, 10);
+  graph_for_bfs->push_node(graph_for_bfs, 10);
   for(int i = 0; i < 10; i++){
     l_list->push(l_list, i, 9-i);
     stack->push(stack, i, 9-i);
     queue->push(queue, i, 9-i);
     prior_queue->push(prior_queue, i, 9-i);
   }
+  graph_for_bfs->push_edge(graph_for_bfs, 0, 1, 1);
+  graph_for_bfs->push_edge(graph_for_bfs, 0, 1, 2);
+  graph_for_bfs->push_edge(graph_for_bfs, 1, 1, 3);
+  graph_for_bfs->push_edge(graph_for_bfs, 1, 1, 4);
+  graph_for_bfs->push_edge(graph_for_bfs, 2, 1, 5);
+  graph_for_bfs->push_edge(graph_for_bfs, 2, 1, 6);
+  graph_for_bfs->push_edge(graph_for_bfs, 3, 1, 7);
+  graph_for_bfs->push_edge(graph_for_bfs, 3, 1, 8);
+  graph_for_bfs->push_edge(graph_for_bfs, 4, 1, 9);
+
+  graph_for_dfs->push_edge(graph_for_dfs, 0, 1, 1);
+  graph_for_dfs->push_edge(graph_for_dfs, 0, 1, 2);
+  graph_for_dfs->push_edge(graph_for_dfs, 1, 1, 3);
+  graph_for_dfs->push_edge(graph_for_dfs, 1, 1, 4);
+  graph_for_dfs->push_edge(graph_for_dfs, 2, 1, 5);
+  graph_for_dfs->push_edge(graph_for_dfs, 2, 1, 6);
+  graph_for_dfs->push_edge(graph_for_dfs, 3, 1, 7);
+  graph_for_dfs->push_edge(graph_for_dfs, 3, 1, 8);
+  graph_for_dfs->push_edge(graph_for_dfs, 4, 1, 9);
+   
+  BFS(graph_for_bfs);
+  printf("BFS\n");
+ 
+  DFS(graph_for_dfs);
+  printf("DFS\n");
+
   l_list->read_all(l_list);
-  printf("\n");
+  printf("LINKED LIST\n");
+
   stack->pop_all(stack);
-  printf("\n");
+  printf("STACK\n");
+
   queue->pop_all(queue);
-  printf("\n");
+  printf("QUEUE\n");
+
   prior_queue->pop_all(prior_queue);
+  printf("PRIOR QUEUE\n");
+
   // int array
   int *array = (int *) malloc(sizeof(int) * 6);
   array[0] = 5;
